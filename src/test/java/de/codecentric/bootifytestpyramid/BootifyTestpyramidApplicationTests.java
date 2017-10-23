8be82ef4e497702ae9aad9f75d4118403b63e04f
Benedikt Ritter <beneritter@gmail.com>
2017-10-23 21:13:34 +0200

    Use JUnit 5 (hope this is a good idea...)

--- src/test/java/de/codecentric/bootifytestpyramid/BootifyTestpyramidApplicationTests.java
+++ src/test/java/de/codecentric/bootifytestpyramid/BootifyTestpyramidApplicationTests.java
@@ -1,16 +1,16 @@
 package de.codecentric.bootifytestpyramid;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class BootifyTestpyramidApplicationTests {
+@ExtendWith(SpringExtension.class)
+class BootifyTestpyramidApplicationTests {
 
     @Test
-    public void contextLoads() {
+    void contextLoads() {
     }
 
 }
