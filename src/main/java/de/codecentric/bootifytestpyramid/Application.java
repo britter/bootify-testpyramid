5d887e60b59d3ea47841f0c2eebcdf81c66c35c8
Benedikt Ritter <beneritter@gmail.com>
2017-10-23 21:13:58 +0200

    Rename main application class

--- src/main/java/de/codecentric/bootifytestpyramid/Application.java
+++ src/main/java/de/codecentric/bootifytestpyramid/Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BootifyTestpyramidApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(BootifyTestpyramidApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
