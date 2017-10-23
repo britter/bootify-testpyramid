cf27c825137068d3edf948a98af9779b5fc993d6
Benedikt Ritter <beneritter@gmail.com>
2017-10-23 21:05:45 +0200

    Initial commit

---
+++ src/main/java/de/codecentric/bootifytestpyramid/BootifyTestpyramidApplication.java
@@ -0,0 +1,12 @@
+package de.codecentric.bootifytestpyramid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifyTestpyramidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootifyTestpyramidApplication.class, args);
+	}
+}
